aeedc92d9d811cdff71e1c7be0836c9c6aa2d29e
Yusuf Çağlar <faf7faf7@gmail.com>
2021-03-29 22:24:13 +0300

    Initial Commit

---
+++ Touchdown/TouchdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchdownApp.swift
+//  Touchdown
+//
+//  Created by Yusuf ÇAĞLAR on 29/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct TouchdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
